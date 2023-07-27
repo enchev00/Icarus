92a560a21efc37d34427488b070bc43ac3a267c3
Stoycho <wickedsick.cs@abv.bg>
2023-07-27 12:37:15 +0300

    Add files via upload
    IcarusCode

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿
+using System.ComponentModel;
+using System.Linq;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Fill in your Array: ");
+        int[] plane = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+        Console.WriteLine("Your Starting Position: ");
+        int ordinalIndex = int.Parse(Console.ReadLine());
+        string stopword = "Supernova";
+        int damage = 1;
+        do
+        {
+            Console.WriteLine("Your input command: ");
+            string[] input = Console.ReadLine().Split(" ").ToArray();
+
+            if (input[0] == stopword)
+            {
+                break;
+            }
+            if (input[0] == "left")
+            {
+                for(int i = 0; i < int.Parse(input[1]); i++)
+                { 
+                    ordinalIndex--;
+                    if (ordinalIndex < 0)
+                    {
+                        ordinalIndex = plane.Length - 1;
+                        damage++;
+                    }
+                    plane[ordinalIndex] -= damage;
+                }   
+            }
+            if (input[0] == "right")
+            {
+                for (int i = 0; i < int.Parse(input[1]); i++)
+                {
+                    ordinalIndex++;
+                    if (ordinalIndex > plane.Length -1)
+                    {
+                        ordinalIndex = 0;
+                        damage++;
+                    }
+                    plane[ordinalIndex] -= damage;
+                }
+
+            }
+        }
+        while (true);
+
+        Console.WriteLine("Current state of the Array: ");
+        for (int i = 0; i < plane.Length; i++)
+        {
+            Console.Write(plane[i] + " ");
+        }
+    }
+}
+
